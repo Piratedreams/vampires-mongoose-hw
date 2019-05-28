2b1b70560984ede71dc5ef9a7e556438d3700b19
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-05-28 12:23:20 -0600

    fixed mongoose homework

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+const Vampire = require('./models/vampire')
 
-const Vampire = require('vampire');
+const vampire = require('Vampire');
+const vampireCollection = require('vampireCollection');
+
+Vampire.collection.insertMany(vampireData, (err, data) => {
+    console.log("added provided vampire data")
+    mongoose.connection.close();
+});
 
 
+const connectionString = 'mongodb://localhost/vamp'
+mongoose.connect(connectionString, { useNewUrlParser: true })
 
-
-
+const vampireData = require('./populateVampires')
+Vampire.collection.insertMany(vampireData, (err, data) => {
+    console.log("added provided vampire data")
+    mongoose.connection.close();
+});
 
 
 const port = 3000;
-app.listen('port', () => {
+app.listen(3000, () => {
     console.log('vampires be listeing to the servers hum');
 })
--- vampire_app/models/vampire.js
+++ vampire_app/models/vampire.js
@@ -1,16 +1,18 @@
-const vampireSchema = new Schema({
+const mongoose = require('mongoose')
+
+const vampireSchema = new mongoose.Schema({
     name: { type: String, required: true},
-    hair_color: {type: String, required: true},
+    hair_color: {type: String, default: 'blonde'},
     eye_color: {type: String, required: true},
-    dob: {type: Date, required: true},
-    loves: [{type: String, required: true}],
-    location: {type: String, required: true},
-    gender: {type: String, required: true, maxLength: 1},
-    victims: {type: Number, required: true, minNumber: 1}
+    dob: {type: Date,},
+    loves: [{type: String, }],
+    location: {type: String, },
+    gender: {type: String, maxLength: 1},
+    victims: {type: Number, min:  1 }
 })
 // a schema is short for schematic so it's like a blueprint for objects just like a class
 // but it works differently since these are forms that must be filled out and answered.
 // essentially like if you were to fill out our card information for a payment
 // that would be filling in the data from a shcema.
 
-module.exports = vampire;
+module.exports = mongoose.model('Vampire', vampireSchema)
