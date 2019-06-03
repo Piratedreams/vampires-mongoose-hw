1763fd08b52b023f14b1f691af380ed6299ce3f4
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-06-03 10:47:51 -0600

    added the queries

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -21,6 +21,118 @@ Vampire.collection.insertMany(vampireData, (err, data) => {
 });
 
 
+
+
+vampire.find({ gender: 'f' }, (err, createdVamp) => {
+        if(err){
+            console.log(err);
+        } else {
+            console.log(createdVamp);
+        }
+    });
+
+vampire.find({ victims: { $gt: '500' } }, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+});
+
+vampire.find({ victims: { $lte: '150' } }, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+vampire.find({victims: {$ne: '210234'}}, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+vampire.find({ victims: { $gt: '150', $lt: '500' } }, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+
+vampire.find({title: {$exists: true}}, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+vampire.find({victims: {$exists: false}}, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+vampire.find({victims: {$exists: false}, title: {$exists: true}}, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+vampire.find({victims: {$exists: true}, victims: {$gt: 1000}}, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+
+//  Or selection
+
+vampire.find({ $or: [{ location: 'New York, New York, US' }, { location: 'New Orleans, Louisiana, US' }] }, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+
+vampire.find({ $or: [{ loves: 'brooding' }, { loves: 'being tragic' }] }, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+vampire.find({$or: [{victims: {$gt: 1000}}, {loves: 'marshmallows'}]}, (err, createdVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+vampire.find({ $or: [{ hair_color: 'red' }, { eye_color: 'green' }] }, (err, createdVamp) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(createdVamp);
+    }
+}); 
+
+
 const port = 3000;
 app.listen(3000, () => {
     console.log('vampires be listeing to the servers hum');
